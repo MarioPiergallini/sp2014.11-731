b60422fa3b201ac4c1eacf7df80ed40beb70499c
Mario Piergallini <mario.p.piergallini@gmail.com>
2014-05-02 02:29:34 -0400

    improved output

--- hw4/README.md
+++ hw4/README.md
@@ -1,3 +1,5 @@
 mpiergal & jdoornbo
 
 -We implemented the Pairwise Ranking Optimization
+-We added features of untranslated words, sentence length
+-We also tried sentence length deviation (how much shorter/longer it was than the avg hypothesis) but it did not make any improvement
--- hw4/generateTraining.py
+++ hw4/generateTraining.py
@@ -5,41 +5,53 @@ import re
 if __name__ == '__main__':
     
     #test or dev
-    
     #datatype = "test"
     datatype = "dev"
   
     datafile = open("C:\\Users\\Mario\\Git\\sp2014.11-731\\hw4\data\\"+datatype+".100best")
-    
     meteors = open("C:\\Users\\Mario\\Git\\sp2014.11-731\\hw4\data\\"+datatype+"_meteor.txt")
     
     hypnum = 0
+    avglen = 0.0
+    hypotheses = []
     for line in datafile:
       line = line.strip()
       hyptext = line.split('|||')[1]
-#      feats = [float(v.split('=')[1]) for v in line.split('|||')[2].split()]
       feats = {v.split('=')[0]:float(v.split('=')[1]) for v in line.split('|||')[2].split()}
-#      feats = {}
-#      for feature in line.split('|||')[2].split():
-#        feats[feature.split('=')[0]] = feature.split('=')[1]
       
+      #get sentence length
       words = hyptext.split()
       feats["sentLen"] = len(words)
-      cyrillic = set(unicode("ёъяшертыуиопющэасдфгчйкльжзхцвбнмЁЪЯШЕРТЫУИОПЮЩЭАСДФГЧЙКЛЬЖЗХЦВБНМ"))
-      cyrils = 0
+      #find words with Cyrillic letters and count them as untranslated/untransliterated
+      untrans = 0
       for word in words:
-        if set(word) & cyrillic:
-          cyrils += 1
-          print word,
-      #cyrils = re.findall("[\A ][^ ]*[ёъяшертыуиопющэасдфгчйкльжзхцвбнмЁЪЯШЕРТЫУИОПЮЩЭАСДФГЧЙКЛЬЖЗХЦВБНМ][^ ]*[\B ]",hyptext,re.UNICODE)
-      #feats["untrans"] = len(re.findall("[\A ][^ ]*[ёъяшертыуиопющэасдфгчйкльжзхцвбнмЁЪЯШЕРТЫУИОПЮЩЭАСДФГЧЙКЛЬЖЗХЦВБНМ][^ ]*[\B ]",hyptext,re.UNICODE))
-      feats["untrans"] = cyrils
-      if feats["untrans"] > 1:
-        print hyptext
-        print feats
-        hypnum += 1
+        if re.search("[^\w\.\,'\"\?\!;()%\-\*:/$&ščťáéíóúüöæåøхapѕ[\]\+\#]",word,re.UNICODE):
+          untrans += 1
+      feats["untrans"] = untrans
+      
+      #print hypothesis and features to check
+#      print hyptext
+#      print feats
+      
+      hypotheses.append((hyptext,feats))
+      hypnum += 1
       if hypnum > 200:
         break
+    
+    for i in range(0,len(hypotheses)):
+      if i%100 == 0:
+        if i != 0:
+          avglen = float(totLen)/100
+          print "\nAverage length for translation",i/100,":",avglen
+          for j in range(i-100,i):
+            hypotheses[j][1]["compLength"] = float(hypotheses[j][1]["sentLen"])/avglen
+            
+            print hypotheses[j][0]
+            print hypotheses[j][1]
+      
+        totLen = 0
+      
+      totLen += hypotheses[i][1]["sentLen"]
     
     datafile.close()
     meteors.close()
